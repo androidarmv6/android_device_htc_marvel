7a8039bdc747d247c866c679079886bd7c3d6f1b
AndroidMeda <hephappy@gmail.com>
2014-06-16 09:37:25 +0200

    Revert "marvel: release BETA1"
    This reverts commit 21bde82bd4539a404241f1d9eb5d9019573c9ce2.

--- cm.mk
+++ cm.mk
@@ -21,9 +21,6 @@ PRODUCT_DEVICE := marvel
 
 # Release name and versioning
 PRODUCT_RELEASE_NAME := WildfireS
-PRODUCT_VERSION_DEVICE_RELEASE := BETA1
-PRODUCT_VERSION_DEVICE_SPECIFIC := -$(PRODUCT_VERSION_DEVICE_RELEASE)
-CM_EXTRAVERSION := $(PRODUCT_VERSION_DEVICE_RELEASE)
 
 ## Assign the build fingerprint to the build.prop file.
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=cyanogenmod/htc_marvel/marvel:4.4.2/$(BUILD_ID)/v1.5:user/release-keys
