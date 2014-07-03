a440a91eff27b37a74669f3b462fa57bf647060e
olivieer <y.karasangabo@bg-rams.ac.at>
2014-07-03 05:05:17 -0400

    marvel: cleanup cm.mk
    * gsm.mk doesn't (actually) exist, see:
    https://github.com/androidarmv6/android_vendor_cm/blob/cm-11.0/config/gsm.mk
    * There's no need for a custom build fingerprint
    
    Change-Id: Ib0fc6addb27bca619afefd9463a88ef3f9499e79

--- cm.mk
+++ cm.mk
@@ -1,14 +1,11 @@
 # Licensed under the Apache License, Version 2.0.
-# Copyright (C) 2009 The Android Open Source Project
-# Copyright (C) 2011 The CyanogenMod Project
-# Copyright (C) 2014 Benjamin Gwynn <http://xenxier.tk>
-# Re-written by Benjamin Gwynn
+# Copyright (C) 2014 The Android Open Source Project
+# Copyright (C) 2014 The CyanogenMod Project
 
 # Call our device's AOSP device config.
 $(call inherit-product, device/htc/marvel/full_marvel.mk)
 
 # Inherit common CM stuff
-$(call inherit-product, vendor/cm/config/gsm.mk)
 $(call inherit-product, vendor/cm/config/mini.mk)
 
 # Bootanimation
@@ -21,6 +18,3 @@ PRODUCT_DEVICE := marvel
 
 # Release name and versioning
 PRODUCT_RELEASE_NAME := WildfireS
-
-## Assign the build fingerprint to the build.prop file.
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=cyanogenmod/htc_marvel/marvel:4.4.2/$(BUILD_ID)/v1.5:user/release-keys
